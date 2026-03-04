223fcd42035f703ad43848475786c384d535fdd2
Ana Clara B A Bittelbrunn <ana.cbab@aluno.ifsc.edu.br>
2026-03-04 16:27:52 -0300

    Delete prj-javafx2 directory

--- prj-javafx2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module br.edu.ifsc.fln.prjjavafx2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens br.edu.ifsc.fln.prjjavafx1 to javafx.fxml;
-    exports br.edu.ifsc.fln.prjjavafx1;
-}
